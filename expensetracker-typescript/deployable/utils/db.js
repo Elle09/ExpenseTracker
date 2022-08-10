2ba390b278a4bb163b4f2e4808a7270c08d90c59
Elle09 <81345563+Elle09@users.noreply.github.com>
2022-08-10 20:18:31 +0530

    Delete expensetracker-typescript directory

--- expensetracker-typescript/deployable/utils/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const sequelize = require("sequelize");
-const Sequelize = new sequelize('Expense_Tracker', 'root', 'Vera2359', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-module.exports = Sequelize;
--- expensetracker-typescript/workable/utils/db.ts
+++
@@ -1,6 +0,0 @@
-const sequelize=require("sequelize")
-const Sequelize=new sequelize('Expense_Tracker','root','Thisonlyme@1',{
-    dialect:'mysql',
-    host:'localhost'
-});
-module.exports=Sequelize;
