bc30f177d961c3c7e70eb28231e7f4208948c2f6
Elle09 <81345563+Elle09@users.noreply.github.com>
2022-08-13 17:57:25 +0530

    Delete premium-expense directory

--- premium-expense/utils/db.js
+++
@@ -1,8 +0,0 @@
-const sequelize=require("sequelize")
-const dotenv = require('dotenv');
-dotenv.config();
-const Sequelize=new sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
-    dialect:'mysql',
-    host:process.env.DB_HOST
-});
-module.exports=Sequelize;
